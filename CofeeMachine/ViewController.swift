a463b267d8e6244b4635e4ed3a93104d8f0c9c5c
bzz <bizzker@ukr.net>
2018-10-25 12:31:54 +0300

    +buttons on off

--- CofeeMachine/ViewController.swift
+++ CofeeMachine/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayInfo: UILabel!
     
     
+    @IBOutlet var allButtons: [UIButton]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +25,6 @@ class ViewController: UIViewController {
         //let myMachine = CofeeMachine()
         //       // print(myMachine.addWater(500))
         //        myMachine.makeLatte()
-        //        myMachine.addWater(500)
-        //        myMachine.addMilk(150)
-        //        myMachine.addCoffee(100)
         //        myMachine.makeLatte()
         //        myMachine.makeEspreso()
         //        myMachine.check("milk")
@@ -103,15 +102,52 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var toEspreso: UIButton!
+    
+    @IBOutlet weak var toCapuccino: UIButton!
+    
+    @IBOutlet weak var toAmericano: UIButton!
+    
+    @IBOutlet weak var toLatte: UIButton!
+    
+    @IBOutlet weak var toCheck: UIButton!
+    
+    @IBOutlet weak var toAddW: UIButton!
+    
+    @IBOutlet weak var toAddC: UIButton!
+    
+    @IBOutlet weak var toAddM: UIButton!
+    
+    @IBOutlet weak var toTray: UIButton!
     
     @IBAction func powerButton() {
         myMachine.power()
         if !state {
             displayInfo.text = "Кофемашина выключена"
+            toEspreso.setTitleColor(.black, for: .normal)
+            toCapuccino.setTitleColor(.black, for: .normal)
+            toAmericano.setTitleColor(.black, for: .normal)
+            toLatte.setTitleColor(.black, for: .normal)
+            toCheck.setTitleColor(.black, for: .normal)
+            toAddW.setTitleColor(.black, for: .normal)
+            toAddC.setTitleColor(.black, for: .normal)
+            toAddM.setTitleColor(.black, for: .normal)
+            toTray.setTitleColor(.black, for: .normal)
         } else {
+            
+            toEspreso.setTitleColor(.blue, for: .normal)
+            toCapuccino.setTitleColor(.blue, for: .normal)
+            toLatte.setTitleColor(.blue, for: .normal)
+            toCheck.setTitleColor(.blue, for: .normal)
+            toAddW.setTitleColor(.blue, for: .normal)
+            toAddC.setTitleColor(.blue, for: .normal)
+            toAddM.setTitleColor(.blue, for: .normal)
+            toTray.setTitleColor(.blue, for: .normal)
+            toAmericano.setTitleColor(.blue, for: .normal)
             displayInfo.text = "Кофемашина готова к работе!"
         }
     }
+    
     
     
 }
